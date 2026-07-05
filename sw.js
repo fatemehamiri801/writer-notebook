cfe3fa8c38ef814645240d24d8e272cec1647a45
fatemehamiri801 <fatemehamiri801@gmail.com>
2026-07-06 01:35:41 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'writer-notebook-v2'; // با هر بار تغییرات، عدد v2 را به v3 تغییر دهید
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
